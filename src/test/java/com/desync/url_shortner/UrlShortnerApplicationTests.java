5e7d81f78e1b5bf938fff5d69460eeaf2da36a29
Vincent Silveira <code.vincentsilveira@gmail.com>
2026-06-29 02:21:08 +0530

    Initialize commit

---
+++ src/test/java/com/desync/url_shortner/UrlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desync.url_shortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
